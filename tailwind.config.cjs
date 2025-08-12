c6e04b4e286b4f50a5a4c3bbc0e586da74cf85b1
Dylan Lgrng <lagrangedylan@gmail.com>
2025-08-12 15:43:20 +0200

    supprr all

--- tailwind.config.cjs
+++
@@ -1,9 +0,0 @@
-/******** Tailwind Config ********/
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
